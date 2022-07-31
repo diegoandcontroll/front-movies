d6d611686379731ffe9f42b863e900ff84920f7a
diegoandcontroll <lukasxdp2@gmail.com>
2022-07-31 11:44:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,111 @@
+function App() {
+  return (
+    <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center',marginTop: '5rem', flexWrap: 'wrap' }}>
+      
+      <div style={{display: 'flex'}}>
+        <div style={{maxWidth: 360}}>
+          <div style={{display: 'block'}}>
+          <img src="https://image.tmdb.org/t/p/w533_and_h300_bestv2/3cyjYtLWCBE1uvWINHFsFnE8LUK.jpg" alt="" width={280} height={180}/>
+          </div>
+          <h2 style={{textAlign: 'center'}}>Castle in the Sky</h2>
+          <p>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</p>
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+            <span>Director: Hayao Miyazaki</span>
+            <span>Producer: Isao Takahata</span>
+          </div>
+          
+        </div>
+        
+        
+      </div>
+
+      <div style={{display: 'flex'}}>
+        <div style={{maxWidth: 360}}>
+          <div style={{display: 'block'}}>
+          <img src="https://image.tmdb.org/t/p/w533_and_h300_bestv2/3cyjYtLWCBE1uvWINHFsFnE8LUK.jpg" alt="" width={280} height={180}/>
+          </div>
+          <h2 style={{textAlign: 'center'}}>Castle in the Sky</h2>
+          <p>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</p>
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+            <span>Director: Hayao Miyazaki</span>
+            <span>Producer: Isao Takahata</span>
+          </div>
+          
+        </div>
+        
+        
+      </div>
+
+      <div style={{display: 'flex'}}>
+        <div style={{maxWidth: 360}}>
+          <div style={{display: 'block'}}>
+          <img src="https://image.tmdb.org/t/p/w533_and_h300_bestv2/3cyjYtLWCBE1uvWINHFsFnE8LUK.jpg" alt="" width={280} height={180}/>
+          </div>
+          <h2 style={{textAlign: 'center'}}>Castle in the Sky</h2>
+          <p>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</p>
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+            <span>Director: Hayao Miyazaki</span>
+            <span>Producer: Isao Takahata</span>
+          </div>
+          
+        </div>
+        
+        
+      </div>
+
+      <div style={{display: 'flex', padding: '1rem 0'}}>
+        <div style={{maxWidth: 360}}>
+          <div style={{display: 'block'}}>
+          <img src="https://image.tmdb.org/t/p/w533_and_h300_bestv2/3cyjYtLWCBE1uvWINHFsFnE8LUK.jpg" alt="" width={280} height={180}/>
+          </div>
+          <h2 style={{textAlign: 'center'}}>Castle in the Sky</h2>
+          <p>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</p>
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+            <span>Director: Hayao Miyazaki</span>
+            <span>Producer: Isao Takahata</span>
+          </div>
+          
+        </div>
+        
+        
+      </div>
+
+      <div style={{display: 'flex'}}>
+        <div style={{maxWidth: 360}}>
+          <div style={{display: 'block'}}>
+          <img src="https://image.tmdb.org/t/p/w533_and_h300_bestv2/3cyjYtLWCBE1uvWINHFsFnE8LUK.jpg" alt="" width={280} height={180}/>
+          </div>
+          <h2 style={{textAlign: 'center'}}>Castle in the Sky</h2>
+          <p>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</p>
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+            <span>Director: Hayao Miyazaki</span>
+            <span>Producer: Isao Takahata</span>
+          </div>
+          
+        </div>
+        
+        
+      </div>
+
+      <div style={{display: 'flex'}}>
+        <div style={{maxWidth: 360}}>
+          <div style={{display: 'block'}}>
+          <img src="https://image.tmdb.org/t/p/w533_and_h300_bestv2/3cyjYtLWCBE1uvWINHFsFnE8LUK.jpg" alt="" width={280} height={180}/>
+          </div>
+          <h2 style={{textAlign: 'center'}}>Castle in the Sky</h2>
+          <p>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</p>
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+            <span>Director: Hayao Miyazaki</span>
+            <span>Producer: Isao Takahata</span>
+          </div>
+          
+        </div>
+        
+        
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
