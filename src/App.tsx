96c5a7f566dc8efbf931033e34e2bb31f327b615
diegoandcontroll <lukasxdp2@gmail.com>
2022-07-31 17:37:58 -0300

    add dockerfile and docker-compose

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
           <div style={{display: 'block'}}>
           <img src="https://image.tmdb.org/t/p/w533_and_h300_bestv2/3cyjYtLWCBE1uvWINHFsFnE8LUK.jpg" alt="" width={280} height={180}/>
           </div>
-          <h2 style={{textAlign: 'center'}}>Castle in the Sky</h2>
+          <h2 style={{textAlign: 'center'}}>Castle in the Sky 3</h2>
           <p>The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.</p>
           <div style={{display: 'flex', flexDirection: 'column'}}>
             <span>Director: Hayao Miyazaki</span>
